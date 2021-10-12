eabddafacddb0a5e13b764067bea30e989bf904c
Luis Felipe Almeida Nogueira <82065199+luis-fnogueira@users.noreply.github.com>
2021-10-12 17:23:45 -0300

    Add files via upload

---
+++ open_browser.py
@@ -0,0 +1,16 @@
+import webbrowser
+from tkinter import *
+
+
+root = Tk( )
+
+root.title('Abrir browser')
+root.geometry('300x200')
+
+
+def google():
+    webbrowser.open('www.google.com')
+
+
+my_google = Button(root, text='Abrir o Google', command=google).pack(pady=20)
+root.mainloop()
